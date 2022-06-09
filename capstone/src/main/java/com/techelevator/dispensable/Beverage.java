db4cda8a09d2d753b78f070eea7a43eaea348d87
Mark Baron <mark_baron@outlook.com>
2022-06-09 16:56:49 -0400

    Final Day 1

--- capstone/src/main/java/com/techelevator/dispensable/Beverage.java
+++ capstone/src/main/java/com/techelevator/dispensable/Beverage.java
@@ -37,13 +37,14 @@ public class Beverage implements Dispensable {
     public String dispense(String itemLocation) {
         inventory.initializeInventory();
         quantity--;
-       return inventory.getItemInventory().get(itemLocation).getName() + "|" + inventory.getItemInventory().get(itemLocation).getPrice() + "|" + BEV_MESSAGE;
+        return inventory.getItemInventory().get(itemLocation).getName() + "|" + inventory.getItemInventory().get(itemLocation).getPrice() + "|" + BEV_MESSAGE;
 
     }
 
     public int getQuantity() {
         return quantity;
     }
+
 
     @Override
     public String getType() {
